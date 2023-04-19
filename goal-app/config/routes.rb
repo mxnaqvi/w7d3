d9d6ae61015deae2488c88a3688bf1dfe5da1b24
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-04-19 07:21:53 -0400

    finished controllers

--- goal-app/config/routes.rb
+++ goal-app/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show, :create, :destroy]
+  resource :session, only: [:create, :destroy]
 end

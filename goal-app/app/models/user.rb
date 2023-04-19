b733f024de6e69e3152ba27c7fea1b1279df0b7e
Matt Pierson <matt.pierson789@gmail.com>
2023-04-19 15:03:39 -0400

    user model

--- goal-app/app/models/user.rb
+++ goal-app/app/models/user.rb
@@ -1,2 +1,62 @@
 class User < ApplicationRecord
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    
+    before_validation: :ensure_session_token
+
+    attr_accessor :password 
+
+    #SPIRE 
+
+    #FIGVABERBR
+
+
+    def self.find_by_credentials(username, password)
+        
+        @user = User.new(username, password)
+
+        if user && user.is_password?(password)
+            user
+        else 
+            nil
+        end
+
+    end 
+
+
+    def password=(password)
+
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password 
+
+    end 
+
+    def is_password?
+        password_object = BCryptoPassword.new(self.password_digest)
+
+         password_object.is_password?(password)
+
+    end 
+
+    def reset_session_token
+
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+
+    end 
+
+
+
+    def ensure_session_token
+
+    self.session_token ||= SecureRandom::urlsafe_base64
+
+    end 
+            
+
+    # after_initialize :ensure_session_token
+    
 end
